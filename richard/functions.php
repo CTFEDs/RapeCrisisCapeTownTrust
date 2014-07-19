b47f3c8c9cc0afe4888030b567bbea472f605979
Richard Korebrits <richard@korebrits.com>
2014-07-19 10:32:07 +0200

    Initial commit

---
+++ richard/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+class RccttManager
+{
+
+  public function __construct(){
+    $this->initWordpress();
+  }
+
+  private function initWordpress(){
+
+  }
+
+  public function getRandomHeaderImageHtml(){
+    getRandomImageUrl();
+  }
+
+  private function getRandomImageUrl(){
+
+  }
+
+}
+
+$RccttManager = new RccttManager();
