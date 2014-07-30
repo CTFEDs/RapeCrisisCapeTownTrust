dc8c4bdeef46526690a0bb65b3bd11b6ef7d1bab
KiFFiE Liversage <mrkiffie@gmail.com>
2014-07-30 22:05:57 +0200

    Registering theme features
    - Fixing a typo
    - Adding post thumbnail support
    - Adding "Primary navigation" menu location
    - Adding search, gallery and caption to components that can support
      html5

--- ctfed-rapecrisis/functions.php
+++ ctfed-rapecrisis/functions.php
@@ -1,8 +1,41 @@
 <?php
+
 /**
  * @subpackage ctfed-rapecrisis
  *
- * @author ctfedsS
+ * @author ctfeds
  *
  * @link http://ctfeds.org/
  */
+
+
+/**
+ * Set up theme defaults and registers support for various WordPress features.
+ *
+ * @since 1.0.0
+ *
+ * @uses add_theme_support
+ * @uses register_nav_menus
+ * @uses __
+ * @uses add_theme_support
+ *
+ * @return void
+ */
+function ctfed_rapecrisis_setup() {
+
+    /**
+     * Ensure that the theme supports post thumbnails
+     */
+    add_theme_support('post-thumbnails');
+
+    /**
+     * Register menu locations
+     */
+    register_nav_menus(array('primary' => __('Primary navigation', 'ctfed-rapecrisis'),));
+
+    /*
+     * Switch default core markup for search form to output valid HTML5.
+    */
+    add_theme_support('html5', array('search-form', 'gallery', 'caption'));
+}
+add_action('after_setup_theme', 'ctfed_rapecrisis_setup');
