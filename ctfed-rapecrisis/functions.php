b1f8acb4f9f11aba2ca58d1f795d8972c5bcd6bd
KiFFiE Liversage <mrkiffie@gmail.com>
2014-07-30 23:12:12 +0200

    Adding enqueue functions for scripts and styles
    - These actions are currently disabled to prevent 404's in logs until
      the build is configured

--- ctfed-rapecrisis/functions.php
+++ ctfed-rapecrisis/functions.php
@@ -8,6 +8,7 @@
  * @link http://ctfeds.org/
  */
 
+define('CTFEDS_RAPECRISIS_VERSION', '1.0.0');
 
 /**
  * Set up theme defaults and registers support for various WordPress features.
@@ -39,3 +40,39 @@ function ctfed_rapecrisis_setup() {
     add_theme_support('html5', array('search-form', 'gallery', 'caption'));
 }
 add_action('after_setup_theme', 'ctfed_rapecrisis_setup');
+
+
+/**
+ * Enqueue the theme's scripts
+ *
+ * @since 1.0.0
+ *
+ * @uses wp_enqueue_script
+ * @uses get_template_directory_uri
+ *
+ * @return void
+ */
+function ctfed_rapecrisis_enqueue_scripts(){
+
+    wp_enqueue_script('rapecrisis-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), CTFEDS_RAPECRISIS_VERSION, true);
+
+}
+// add_action('wp_enqueue_scripts', 'ctfed_rapecrisis_enqueue_scripts');
+
+
+/**
+ * Enqueue the theme's stylesheet
+ *
+ * @since 1.0.0
+ *
+ * @uses wp_enqueue_style
+ * @uses get_stylesheet_directory_uri
+ *
+ * @return void
+ */
+function ctfed_rapecrisis_enqueue_styles(){
+
+    wp_enqueue_style('rapecrisis-styles', get_stylesheet_directory_uri() . '/assets/css/style.css', false, CTFEDS_RAPECRISIS_VERSION, 'all');
+
+}
+// add_action('wp_enqueue_scripts', 'ctfed_rapecrisis_enqueue_styles');
