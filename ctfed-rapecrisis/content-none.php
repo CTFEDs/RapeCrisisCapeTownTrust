6a8efecb727963194a771cacdee7086c678224fa
KiFFiE Liversage <mrkiffie@gmail.com>
2014-07-30 23:07:55 +0200

    Adding basic markup to the content templates

--- ctfed-rapecrisis/content-none.php
+++ ctfed-rapecrisis/content-none.php
@@ -6,3 +6,21 @@
  *
  * @link http://ctfeds.org/
  */
+?>
+<header class="page-header">
+    <h1 class="page-title">Nothing Found</h1>
+</header>
+
+<div class="page-content">
+    <?php if ( is_search() ) { ?>
+
+    <p>Sorry, but nothing matched your search terms. Please try again with some different keywords.</p>
+    <?php get_search_form(); ?>
+
+    <?php } else { ?>
+
+    <p>It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.</p>
+    <?php get_search_form(); ?>
+
+    <?php } ?>
+</div>
--- ctfed-rapecrisis/content.php
+++ ctfed-rapecrisis/content.php
@@ -6,3 +6,37 @@
  *
  * @link http://ctfeds.org/
  */
+?>
+<article <?php post_class(); ?>>
+
+    <header class="entry-header">
+        <h1 class="entry-title">
+            <?php if (is_single()){?>
+                <?php the_title(); ?>
+            <?php } else { ?>
+                <a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a>
+            <?php } ?>
+        </h1>
+
+        <div class="entry-meta">
+            <?php
+                edit_post_link('Edit');
+            ?>
+        </div>
+    </header>
+
+    <?php if ( is_single() ) { ?>
+    <div class="entry-content">
+        <?php
+            the_content();
+            wp_link_pages();
+        ?>
+    </div>
+    <?php } else { ?>
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div>
+    <?php } ?>
+    <footer class="entry-meta"></footer>
+
+</article>
